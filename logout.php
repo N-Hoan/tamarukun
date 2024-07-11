35121d6e87093bd10503c45941e040cf5de3135b
Daichi012345 <168045120+Daichi012345@users.noreply.github.com>
2024-07-11 09:30:52 +0900

    ログアウト処理

---
+++ logout.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+// セッション変数をすべて解除する
+$_SESSION = array();
+
+// セッションを破壊する
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+session_destroy();
+
+// ログインページにリダイレクト
+header("Location: login.php");
+exit;
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
